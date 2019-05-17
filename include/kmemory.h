9b9033e0d1c2dfcab691025695d6729db5457cda
Krystian Stasiowski <sdkrystian@gmail.com>
2019-05-17 09:09:21 -0400

    Fixed memory moving utilities

--- include/kmemory.h
+++ include/kmemory.h
@@ -1,25 +1,122 @@
 #pragma once
 #include <memory>
+#include <utility>
+#include <type_traits>
 
 namespace kstd
 {
-  template<typename InputIt, typename ForwardIt>
-  ForwardIt uninitialized_move_if_noexcept(InputIt first, InputIt last, ForwardIt d_first)
+  namespace detail
   {
-    if constexpr (std::is_nothrow_move_constructible_v<std::iterator_traits<InputIt>::value_type>)
-      return std::uninitialized_move(first, last, d_first);
-    else
-      return std::uninitialized_copy(first, last, d_first);
-  }
+    //template<typename T, typename U>
+    //T* uninitialized_move_range_optimal(T* first, T* last, U* d_first) // pointers so it works with memcpy
+    //{
+    //  if constexpr (std::is_trivial_v<T>)
+    //    return static_cast<T*>(std::memcpy(d_first, first, (last - first) * sizeof(T)));
+    //  else if constexpr (std::is_nothrow_move_constructible_v<T>)
+    //    return std::uninitialized_move(first, last, d_first);
+    //  else
+    //    return std::uninitialized_copy(first, last, d_first);
+    //}
 
-  //template<typename InputIt, typename ForwardIt>
-  //ForwardIt move_range_optimal(InputIt first, InputIt last, ForwardIt d_first)
-  //{
-  //  if constexpr (std::is_trivial_v<std::iterator_traits<InputIt>::value_type>)
-  //    return std::memcpy(d_first, first, last - first); // gah UB
-  //  else if constexpr (std::is_nothrow_move_constructible_v<std::iterator_traits<InputIt>::value_type>)
-  //    return std::uninitialized_move(first, last, d_first);
-  //  else
-  //    return std::uninitialized_copy(first, last, d_first);
-  //}
+    //template<typename T, typename U>
+    //T* move_range_optimal(T* first, T* last, U* d_first) // pointers so it works with memcpy
+    //{
+    //  if constexpr (std::is_trivial_v<T>)
+    //    return static_cast<T*>(std::memcpy(d_first, first, (last - first) * sizeof(T)));
+    //  else if constexpr (std::is_nothrow_move_constructible_v<T>)
+    //    return std::move(first, last, d_first);
+    //  else
+    //    return std::copy(first, last, d_first);
+    //}
+
+    //template<typename T, typename U>
+    //T* move_range_optimal_backward(T* first, T* last, U* d_first) // pointers so it works with memcpy
+    //{
+    //  if constexpr (std::is_trivial_v<T>)
+    //    return static_cast<T*>(std::memmove(d_first, first, (last - first) * sizeof(T)));
+    //  else if constexpr (std::is_nothrow_move_constructible_v<T>)
+    //    return std::move_backward(first, last, d_first);
+    //  else
+    //    return std::copy_backward(first, last, d_first);
+    //}
+
+    //template<typename T, typename U>
+    //T* copy_range_optimal(T* first, T* last, U* d_first) // pointers so it works with memcpy
+    //{
+    //  if constexpr (std::is_trivial_v<T>)
+    //    return static_cast<T*>(std::memcpy(d_first, first, (last - first) * sizeof(T)));
+    //  else
+    //    return std::copy(first, last, d_first);
+    //}
+
+    //template<typename T, typename U>
+    //T* uninitialized_copy_range_optimal(T* first, T* last, U* d_first) // pointers so it works with memcpy
+    //{
+    //  if constexpr (std::is_trivial_v<T>)
+    //    return static_cast<T*>(std::memcpy(d_first, first, (last - first) * sizeof(T)));
+    //  else
+    //    return std::uninitialized_copy(first, last, d_first);
+    //}
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+    template<typename InputIterator, typename OutputIterator>
+    OutputIterator uninitialized_move_range_optimal(InputIterator first, InputIterator last, OutputIterator d_first)
+    {
+      using T = typename std::iterator_traits<InputIterator>::value_type;
+      if constexpr (std::is_trivial_v<T>)
+        return OutputIterator(std::memcpy(d_first, first, (last - first) * sizeof(T)));
+      else if constexpr (std::is_nothrow_move_constructible_v<T>)
+        return std::uninitialized_move(first, last, d_first);
+      else
+        return std::uninitialized_copy(first, last, d_first);
+    }
+
+    template<typename InputIterator, typename OutputIterator>
+    OutputIterator move_range_optimal(InputIterator first, InputIterator last, OutputIterator d_first)
+    {
+      using T = typename std::iterator_traits<InputIterator>::value_type;
+      if constexpr (std::is_trivial_v<T>)
+        return OutputIterator(std::memcpy(d_first, first, (last - first) * sizeof(T)));
+      else if constexpr (std::is_nothrow_move_constructible_v<T>)
+        return std::move(first, last, d_first);
+      else
+        return std::copy(first, last, d_first);
+    }
+
+    template<typename InputIterator, typename OutputIterator>
+    OutputIterator move_range_optimal_backward(InputIterator first, InputIterator last, OutputIterator d_first)
+    {
+      using T = typename std::iterator_traits<InputIterator>::value_type;
+      if constexpr (std::is_trivial_v<T>)
+        return OutputIterator(std::memmove(d_first, first, (last - first) * sizeof(T)));
+      else if constexpr (std::is_nothrow_move_constructible_v<T>)
+        return std::move_backward(first, last, d_first);
+      else
+        return std::copy_backward(first, last, d_first);
+    }
+
+    template<typename InputIterator, typename OutputIterator>
+    OutputIterator copy_range_optimal(InputIterator first, InputIterator last, OutputIterator d_first)
+    {
+      using T = typename std::iterator_traits<InputIterator>::value_type;
+      if constexpr (std::is_trivial_v<T>)
+        return OutputIterator(std::memcpy(d_first, first, (last - first) * sizeof(T)));
+      else
+        return std::copy(first, last, d_first);
+    }
+
+    template<typename InputIterator, typename OutputIterator>
+    OutputIterator uninitialized_copy_range_optimal(InputIterator first, InputIterator last, OutputIterator d_first)
+    {
+      using T = typename std::iterator_traits<InputIterator>::value_type;
+      if constexpr (std::is_trivial_v<T>)
+        return OutputIterator(std::memcpy(d_first, first, (last - first) * sizeof(T)));
+      else
+        return std::uninitialized_copy(first, last, d_first);
+    }
+  }
 }
--- include/kvector.h
+++ include/kvector.h
@@ -9,57 +9,6 @@ namespace kstd
 {
   namespace detail
   {
-    template<typename T, typename U>
-    T* uninitialized_move_range_optimal(T* first, T* last, U* d_first) // pointers so it works with memcpy
-    {
-      if constexpr (std::is_trivial_v<T>)
-        return static_cast<T*>(std::memcpy(d_first, first, (last - first) * sizeof(T)));
-      else if constexpr(std::is_nothrow_move_constructible_v<T>)
-        return std::uninitialized_move(first, last, d_first);
-      else
-        return std::uninitialized_copy(first, last, d_first);
-    }
-
-    template<typename T, typename U>
-    T* move_range_optimal(T* first, T* last, U* d_first) // pointers so it works with memcpy
-    {
-      if constexpr (std::is_trivial_v<T>)
-        return static_cast<T*>(std::memcpy(d_first, first, (last - first) * sizeof(T)));
-      else if constexpr (std::is_nothrow_move_constructible_v<T>)
-        return std::move(first, last, d_first);
-      else
-        return std::copy(first, last, d_first);
-    }
-
-    template<typename T, typename U>
-    T* move_range_optimal_backward(T* first, T* last, U* d_first) // pointers so it works with memcpy
-    {
-      if constexpr (std::is_trivial_v<T>)
-        return static_cast<T*>(std::memmove(d_first, first, (last - first) * sizeof(T)));
-      else if constexpr (std::is_nothrow_move_constructible_v<T>)
-        return std::move_backward(first, last, d_first);
-      else
-        return std::copy_backward(first, last, d_first);
-    }
-
-    template<typename T, typename U>
-    T* copy_range_optimal(T* first, T* last, U* d_first) // pointers so it works with memcpy
-    {
-      if constexpr (std::is_trivial_v<T>)
-        return static_cast<T*>(std::memcpy(d_first, first, (last - first) * sizeof(T)));
-      else
-        return std::copy(first, last, d_first);
-    }
-
-    template<typename T, typename U>
-    T* uninitialized_copy_range_optimal(T* first, T* last, U* d_first) // pointers so it works with memcpy
-    {
-      if constexpr (std::is_trivial_v<T>)
-        return static_cast<T*>(std::memcpy(d_first, first, (last - first) * sizeof(T)));
-      else
-        return std::uninitialized_copy(first, last, d_first);
-    }
-
     template<typename T, typename = void>
     struct allocator_base
     {
@@ -323,14 +272,14 @@ namespace kstd
     iterator insert(const_iterator pos, size_type count, const T& value)
     {
       size_type emplaced_pos = pos - begin();
-      size_type count = last - first;
+      //size_type count = last - first;
       if (!shift_elements_right(emplaced_pos, count))
       {
 
       }
       else
       {
-        detail::uninitialized_copy_range_optimal(first, last, data_ + emplaced_pos);
+        //detail::uninitialized_copy_range_optimal(first, last, data_ + emplaced_pos);
       }
     }
 
